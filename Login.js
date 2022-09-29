b8c7f023bee14903c2a2322d9adc12f43c9fd74d
devloper-kartik <112373403+devloper-kartik@users.noreply.github.com>
2022-09-29 15:51:55 +0530

    Add files via upload

--- Login.js
+++ Login.js
@@ -1,51 +1,84 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer, useState } from "react";
+import Card from "../UI/Card/Card";
+import classes from "./Login.module.css";
+import Button from "../UI/Button/Button";
 
-import Card from '../UI/Card/Card';
-import classes from './Login.module.css';
-import Button from '../UI/Button/Button';
+const emailReducer=(state,action)=>{
+  if(action.type==='ADD_EMAIL'){
+    return {value:action.val,isVaild:action.val.includes('@')} 
+  }
+  if(action.type==='CHECK_EMAIL'){
+    return {value:state.value,isVaild:state.value.includes('@')} 
+  }
+  return {value:'',isVaild:undefined}
+};
+
+const passwordReducer=(state,action)=>{
+  if(action.type==='ADD_PASSWORD'){
+    return {value:action.val,isVaild:action.val>=6};
+  }
+  if(action.type==='CHECK_PASSWORD'){
+    return {value:state.value,isVaild:state.value>=6};
+  }
+  return {value:'',isVaild:undefined};
+}
+
+// const clgReducer=(state,action)=>{
+//   if(action.type==='ADD_PASSWORD'){
+//     return {value:action.val,isVaild:action.val>=6};
+//   }
+//   if(action.type==='CHECK_PASSWORD'){
+//     return {value:state.value,isVaild:state.value>=6};
+//   }
+//   return {value:'',isVaild:undefined};
+// }
 
 const Login = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [emailIsValid, setEmailIsValid] = useState();
-  const [enteredPassword, setEnteredPassword] = useState('');
-  const [passwordIsValid, setPasswordIsValid] = useState();
-  const [enteredClg,setClg]=useState('');
-  const [ClgIsValid,setCldValid]=useState();
+  // const [enteredEmail, setEnteredEmail] = useState("");
+  // const [emailIsValid, setEmailIsValid] = useState();
+  // const [enteredPassword, setEnteredPassword] = useState("");
+  // const [passwordIsValid, setPasswordIsValid] = useState();
+  const [enteredClg, setClg] = useState("");
+  const [ClgIsValid, setCldValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
-  useEffect(()=>{
+  const [emailState, dispatchEmail] = useReducer(emailReducer,{value:'',isVaild:undefined});
+  const [passwordState,dispatchPassword]=useReducer(passwordReducer,{value:'',isVaild:undefined});
+  useEffect(() => {
     setFormIsValid(
-      enteredEmail.includes('@') && enteredPassword.trim().length > 6 && enteredClg.length>3
+      emailState.isVaild &&
+        passwordState.isVaild&&
+        enteredClg.length > 3
     );
-  },[enteredEmail,enteredPassword,enteredClg]);
-  
+  }, [emailState,enteredClg,passwordState]);
+
   const emailChangeHandler = (event) => {
-    setEnteredEmail(event.target.value);
+    dispatchEmail({type:'ADD_EMAIL',val:event.target.value});
   };
 
   const passwordChangeHandler = (event) => {
-    setEnteredPassword(event.target.value);
+    dispatchPassword({type:'ADD_PASSWORD',val:event.target.value});
   };
 
   const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes('@'));
+    dispatchEmail({type:'CHECK_EMAIL'});
   };
 
   const validatePasswordHandler = () => {
-    setPasswordIsValid(enteredPassword.trim().length > 6);
+    dispatchPassword({type:'CHECK_PASSWORD'});
   };
 
-  const ClgChangeHandler=(event)=>{
+  const ClgChangeHandler = (event) => {
     setClg(event.target.value);
-  }
+  };
 
-  const validateClgHandler= ()=>{
-    setCldValid(enteredClg.length>3);
+  const validateClgHandler = () => {
+    setCldValid(enteredClg.length > 3);
   };
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(enteredEmail, enteredPassword);
+    props.onLogin(emailState.value, passwordState.value);
   };
 
   return (
@@ -53,39 +86,40 @@ const Login = (props) => {
       <form onSubmit={submitHandler}>
         <div
           className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ''
+            emailState.isVaild === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="email">E-Mail</label>
           <input
             type="email"
             id="email"
-            value={enteredEmail}
+            value={emailState.value}
             onChange={emailChangeHandler}
             onBlur={validateEmailHandler}
           />
         </div>
         <div
-           className={`${classes.control} ${
-            ClgIsValid === false ? classes.invalid : ''
+          className={`${classes.control} ${
+            ClgIsValid === false ? classes.invalid : ""
           }`}
         >
           <label> College Name</label>
-          <input 
+          <input
             type="text"
             onChange={ClgChangeHandler}
-            onBlur={validateClgHandler}  ></input>
+            onBlur={validateClgHandler}
+          ></input>
         </div>
         <div
           className={`${classes.control} ${
-            passwordIsValid === false ? classes.invalid : ''
+            passwordState.isVaild === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="password">Password</label>
           <input
             type="password"
             id="password"
-            value={enteredPassword}
+            value={passwordState.value}
             onChange={passwordChangeHandler}
             onBlur={validatePasswordHandler}
           />
